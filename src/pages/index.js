5b4ea4d714f2046c262d4689e063b6d7d808143a
Roger <rdpender3@gmail.com>
2018-09-01 09:02:51 -0700

    added a . I forgot

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Hi everyone!</h1>
     <p>My name is Roger and I'm a boot camp student at UC Davis. I am currently studying to be a full stack web developer.</p>
-    <p>I'm looking forward to practicing what I've been learning, and to collaborate with everyone I meet at the hackathon. I want to take this opportunity to network and see what I am capable of outside of my boot camp</p>
+    <p>I'm looking forward to practicing what I've been learning, and to collaborate with everyone I meet at the hackathon. I want to take this opportunity to network and see what I am capable of outside of my boot camp.</p>
     <Link to="/page-2/">Page 2</Link>
   </div>
 )
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -5,7 +5,7 @@ import Link from 'gatsby-link'
 const SecondPage = () => (
   <div>
     <h1>Please pick me!</h1>
-    <p><small>And Enea Antonicelli if you can. Link to his page: <a href="https://trusting-kalam-2b07df.netlify.com/" target="_blank">https://trusting-kalam-2b07df.netlify.com/</a></small></p>
+    <p><small>And Enea Antonicelli if you can. Link to his page: <a href="https://trusting-kalam-2b07df.netlify.com/" target="_blank">https://trusting-kalam-2b07df.netlify.com/</a></small>.</p>
     <Link to="/">Go back to the page 1</Link>
   </div>
 )
