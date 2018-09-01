5a44e15fc497d9a3903b88c278305284b145ce07
Roger <rdpender3@gmail.com>
2018-09-01 09:00:24 -0700

    fixed up a bit

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hi everyone!</h1>
-    <p>My name is Roger and I'm a boot camp student.</p>
-    <p>I'm looking forward to practicing what I've been learning, and to learn a ton from the people I meet at the hackathon.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>My name is Roger and I'm a boot camp student at UC Davis. I am currently studying to be a full stack web developer.</p>
+    <p>I'm looking forward to practicing what I've been learning, and to collaborate with everyone I meet at the hackathon. I want to take this opportunity to network and see what I am capable of outside of my boot camp</p>
+    <Link to="/page-2/">Page 2</Link>
   </div>
 )
 
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+
 const SecondPage = () => (
   <div>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1>Please pick me!</h1>
+    <p><small>And Enea Antonicelli if you can. Link to his page: <a href="https://trusting-kalam-2b07df.netlify.com/" target="_blank">https://trusting-kalam-2b07df.netlify.com/</a></small></p>
+    <Link to="/">Go back to the page 1</Link>
   </div>
 )
 
 export default SecondPage
+
+
