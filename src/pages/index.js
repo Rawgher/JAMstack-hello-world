4333ec3097c51ed111afa5ffb83d8d25a7da5896
Roger <rdpender3@gmail.com>
2018-08-31 21:26:16 -0700

    fixed wording

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
+    <h1>Hi everyone!</h1>
     <p>My name is Roger and I'm a boot camp student.</p>
-    <p>I'm looking forward to practicing what I've been learning, and to learn a ton from the people I meet at the hackathon</p>
+    <p>I'm looking forward to practicing what I've been learning, and to learn a ton from the people I meet at the hackathon.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
