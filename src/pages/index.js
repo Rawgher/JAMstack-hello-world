7774f79413e46d4efae9e8bb9d154e091f0b5a31
Roger <rdpender3@gmail.com>
2018-08-31 21:18:21 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>My name is Roger and I'm a boot camp student.</p>
+    <p>I'm looking forward to practicing what I've been learning, and to learn a ton from the people I meet at the hackathon</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
